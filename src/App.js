9b7b52b98018eb12978eb3ea96c55541f33846de
Andi <andi.lin.hy@gmail.com>
2022-12-14 07:40:04 +0800

    feat: search input layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
+import HomePage from 'containers';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <p>React App</p>
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style.scss';
+
+function SearchBar() {
+  return (
+    <div className="search-wrapper">
+      <input className="search-input" onChange={() => {}} />
+      <span className="search-btn" onClick={() => {}}>Search</span>
+    </div>
+  );
+}
+
+export default SearchBar;
